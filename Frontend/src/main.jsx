3276fe409258b0a8a47c89bb5d279274f3f54931
Bejide-123 <isbejide10@gmail.com>
2026-01-09 11:22:09 +0100

    Made it a PWA by adding manifest file and updating index.html

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from 'virtual:pwa-register';
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./Context/themeContext";
+
+if (import.meta.env.PROD) {
+  registerSW({ immediate: true });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
