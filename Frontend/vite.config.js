241d6317fb172be10dc90e0edcb08562d0a61343
Bejide-123 <isbejide10@gmail.com>
2026-01-09 11:34:34 +0100

    updated icons for PWA support

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -19,16 +19,14 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/',
         icons: [
-          {
-            src: '/Scribe.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/Scribe.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: '/Scribe.png', sizes: '72x72', type: 'image/png' },
+          { src: '/Scribe.png', sizes: '96x96', type: 'image/png' },
+          { src: '/Scribe.png', sizes: '128x128', type: 'image/png' },
+          { src: '/Scribe.png', sizes: '144x144', type: 'image/png' },
+          { src: '/Scribe.png', sizes: '152x152', type: 'image/png' },
+          { src: '/Scribe.png', sizes: '192x192', type: 'image/png' },
+          { src: '/Scribe.png', sizes: '384x384', type: 'image/png' },
+          { src: '/Scribe.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
